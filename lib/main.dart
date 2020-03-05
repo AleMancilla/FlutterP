5fd7e539235492edf100f2656dec3fd8796556f5
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-03-05 00:37:18 -0400

    hola mundo con Expanded center

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,74 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-    Center(
-      child: Text(
-        'Hello World',
-        textDirection: TextDirection.ltr,
+class MyAppBar extends StatelessWidget{
+
+  MyAppBar({this.title});
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      height: 76.0,
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      child: Row(
+        children:<Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation Menu',
+            onPressed: null,
+          ),
+
+          Expanded(
+            child: title,
+          ),
+          
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search', 
+            onPressed: null,
+            ),
+        ]
+      )
+    );
+  }
+  
+}
+
+class MyScaffold extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Material(
+      child: Column(
+
+        children: <Widget>[
+          MyAppBar(
+            title: Text(
+              'Example Title', 
+              style: Theme.of(context).primaryTextTheme.title,
+              ),
+          ),
+
+          Expanded(child: Center(
+            child: Text('Hola Mundo'),
+          ),
+          ),
+          
+        ],
       ),
-    ),
-  );//runApp
+    );
+  }
+
+}
+
+void main(){
+  runApp(MaterialApp(
+    title: 'My app',
+    home: MyScaffold(),
+  )
+  );
 }
