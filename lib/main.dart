750180bc29712fbcce1cfc0ffe4e97b82b190965
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-03-06 15:59:38 -0400

    alert dialog action yes no

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyDialog extends StatefulWidget {
   _MyDialogState createState() => new _MyDialogState();
  }
 
-  
+  enum DialogAction{yes,no}
 
 
 
@@ -20,11 +20,20 @@ class _MyDialogState extends State<MyDialog> {
 
   String _inputv = "";
 
+  void _alertResult(DialogAction action){
+    print("tu accion es $action");
+  }
+
   void _showAlert(String dato){
 
     AlertDialog dialogo = new AlertDialog(
       title: new Text("este titulo"),
       content: new Text(dato),
+      actions: <Widget>[
+        new FlatButton(onPressed: (){_alertResult(DialogAction.yes);}, child: new Text("si")),
+        new FlatButton(onPressed: (){_alertResult(DialogAction.no);}, child: new Text("no")),
+        
+      ],
 
     );
     showDialog(context: context, child: dialogo);
